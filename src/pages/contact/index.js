fa8e2a7535059063313679083dbb352465703b9a
l1ngoman <41375558+l1ngoman@users.noreply.github.com>
2019-07-22 13:18:06 -0700

    ATG:: Returning to old, static contact page.

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -6,18 +6,15 @@ class ContactInfo extends Component {
   render() {
     return (
       <main style={page.container}>
-        <ContactForm />
+      <br/><br/><br/><br/>
+      <h2 style={page.sectionTitle}>
+        Please contact me at athomasgarrett@gmail.com with any questions you may have.
+      </h2>
+      <br/>
+      <h2 style={page.sectionTitle}>Thank you!</h2>
      </main>
     );
   }
 }
 
 export default ContactInfo
-
-
-// <br/><br/><br/><br/>
-// <h2 style={page.sectionTitle}>
-//   Please contact me at athomasgarrett@gmail.com with any questions you may have.
-// </h2>
-// <br/>
-// <h2 style={page.sectionTitle}>Thank you!</h2>
